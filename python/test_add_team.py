f52975a72fe44fccaa1f08f33cd07f4289988e7d
Laurent HAN <laurenthan@hotmail.com>
2025-01-28 09:38:53 +0100

    add test same user

--- python/test_add_team.py
+++ python/test_add_team.py
@@ -42,7 +42,62 @@ def test_create_user_with_info(page):
     assert page.get_by_role("cell", name="Name", exact=True).is_visible()
     assert page.get_by_role("cell", name="email@gmail.com", exact=True).is_visible()
 
-def test_address2_become_address1(page):
+def test_create_existing_user(page):
+    user_name = "Name"
+    user_email = "email@gmail.com"
+    user_address = "Test address 1"
+    user_address2 = "Test address 2"
+    user_city = "City test"
+    user_zip = "75000"
+    user_hiring_date = "2025-01-17"
+    user_job_title = "Test"
+
+    page.goto("https://f.se1.hr.dmerej.info/")
+    page.get_by_role("link", name="Add new employee").click()
+    page.get_by_role("textbox", name="Name").click()
+    page.get_by_role("textbox", name="Name").fill(user_name)
+    page.get_by_role("textbox", name="Email").click()
+    page.get_by_role("textbox", name="Email").fill(user_email)
+    page.locator("#id_address_line1").click()
+    page.locator("#id_address_line1").fill(user_address)
+    page.locator("#id_address_line2").click()
+    page.locator("#id_address_line2").fill(user_address2)
+    page.get_by_role("textbox", name="City").click()
+    page.get_by_role("textbox", name="City").fill(user_city)
+    page.get_by_role("spinbutton", name="Zip code").click()
+    page.get_by_role("spinbutton", name="Zip code").fill(user_zip)
+    page.get_by_role("textbox", name="Hiring date").fill(user_hiring_date)
+    page.get_by_role("textbox", name="Job title").click()
+    page.get_by_role("textbox", name="Job title").fill(user_job_title)
+    page.get_by_role("button", name="Add").click()
+
+    assert page.get_by_role("cell", name="Name", exact=True).is_visible()
+    assert page.get_by_role("cell", name="email@gmail.com", exact=True).is_visible()
+
+    page.goto("https://f.se1.hr.dmerej.info/")
+    page.get_by_role("link", name="Add new employee").click()
+    page.get_by_role("textbox", name="Name").click()
+    page.get_by_role("textbox", name="Name").fill(user_name)
+    page.get_by_role("textbox", name="Email").click()
+    page.get_by_role("textbox", name="Email").fill(user_email)
+    page.locator("#id_address_line1").click()
+    page.locator("#id_address_line1").fill(user_address)
+    page.locator("#id_address_line2").click()
+    page.locator("#id_address_line2").fill(user_address2)
+    page.get_by_role("textbox", name="City").click()
+    page.get_by_role("textbox", name="City").fill(user_city)
+    page.get_by_role("spinbutton", name="Zip code").click()
+    page.get_by_role("spinbutton", name="Zip code").fill(user_zip)
+    page.get_by_role("textbox", name="Hiring date").fill(user_hiring_date)
+    page.get_by_role("textbox", name="Job title").click()
+    page.get_by_role("textbox", name="Job title").fill(user_job_title)
+    page.get_by_role("button", name="Add").click()
+
+    email_occurrences = page.locator(f"text={user_email}").count()
+    assert email_occurrences == 1, f"Expected 1 occurrence of email '{user_email}', but found {email_occurrences}. ""The user may have been created twice."
+
+
+def test_address2_not_become_address1(page):
     page.get_by_role("link", name="Add new employee").click()
     page.get_by_role("textbox", name="Name").click()
     page.get_by_role("textbox", name="Name").fill("test")
@@ -70,7 +125,7 @@ def test_address2_become_address1(page):
 
     address1 = page.locator("#id_address_line1").input_value()
     updated_address2 = page.locator("#id_address_line2").input_value()
-    assert updated_address2 != address1, f"Expected address2 to be '{address2}', but got '{updated_address2}' equal to '{address1}'"
+    assert updated_address2 != address1, f"Address 2 ('{updated_address2}') unexpectedly matches Address 1 ('{address1}')."
     
 
 def test_zipcode_cannot_be_negative(page):
