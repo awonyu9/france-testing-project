904aa2603cf80cf9f6d82128f169422f4fc7ab32
Alexandra Wonyu <alexandrawonyu@gmail.com>
2025-01-28 11:11:30 +0100

    refactor existing user test with POM

---
+++ python/models/add_new_employee.py
@@ -0,0 +1,36 @@
+from utils import create_user
+
+
+class AddNewEmployeePage:
+    def getPageInput(self, page, name):
+        return page.locator('input[name="' + name + '"]')
+
+    def __init__(self, page):
+        self.page = page
+
+        self.name_input = self.getPageInput(page, "name")
+        self.email_input = self.getPageInput(page, "email")
+        self.address1_input = self.getPageInput(page, "address_line1")
+        self.address2_input = self.getPageInput(page, "address_line2")
+        self.city_input = self.getPageInput(page, "city")
+        self.zip_input = self.getPageInput(page, "zip_code")
+        self.hiring_date_input = self.getPageInput(page, "hiring_date")
+        self.job_title_input = self.getPageInput(page, "job_title")
+
+        self.add_button = page.get_by_role("button", name="Add")
+
+    def navigate(self):
+        self.page.goto("/add_employee")
+
+    def fill(self, name, email, address1, address2, city, zip, hiring_date, job_title):
+        self.name_input.fill(name)
+        self.email_input.fill(email)
+        self.address1_input.fill(address1)
+        self.address2_input.fill(address2)
+        self.city_input.fill(city)
+        self.zip_input.fill(zip)
+        self.hiring_date_input.fill(hiring_date)
+        self.job_title_input.fill(job_title)
+
+    def add(self):
+        self.add_button.click()
---
+++ python/test_create_existing_user_pom.py
@@ -0,0 +1,30 @@
+from models.add_new_employee import AddNewEmployeePage
+
+
+def test_create_existing_user_pom(page):
+    user_name = "John Doe"
+    user_email = "email@gmail.com"
+    user_address1 = "Address 1"
+    user_address2 = "Address 2"
+    user_city = "Paris"
+    user_zip = "75001"
+    user_hiring_date = "2025-01-17"
+    user_job_title = "Manager"
+
+    add_new_employee_page = AddNewEmployeePage(
+        page)
+    add_new_employee_page.navigate()
+    add_new_employee_page.fill(user_name, user_email, user_address1, user_address2, user_city, user_zip, user_hiring_date, user_job_title
+                               )
+    add_new_employee_page.add()
+
+    assert page.is_visible(f"td:has-text('{user_name}')")
+    assert page.is_visible(f"td:has-text('{user_email}')")
+
+    add_new_employee_page.navigate()
+    add_new_employee_page.fill(user_name, user_email, user_address1, user_address2, user_city, user_zip, user_hiring_date, user_job_title
+                               )
+    add_new_employee_page.add()
+
+    email_occurrences = page.locator(f"text={user_email}").count()
+    assert email_occurrences == 1, f"Expected 1 occurrence of email '{user_email}', but found {email_occurrences}. ""The user may have been created twice."
