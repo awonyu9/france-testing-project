32ce966932e2a94597c0ec26fc96ee568e185aa8
AIphahs <aurelien.ah-sane@efrei.net>
2025-01-28 11:30:58 +0100

    Add team model

---
+++ python/models/add_team.py
@@ -0,0 +1,19 @@
+from utils import getPageInput
+
+
+class AddTeamPage:
+    def __init__(self, page):
+        self.page = page
+
+        self.name_input = getPageInput(page, "name")
+
+        self.add_button = page.get_by_role("button", name="Add")
+
+    def navigate(self):
+        self.page.goto("/add_team")
+
+    def fill(self, name):
+        self.name_input.fill(name)
+
+    def add(self):
+        self.add_button.click()
